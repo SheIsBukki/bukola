3bdfccdcbdb191315d7fae36d84973c6c0078816
Bukola Ogunleye <bukolaogunleye2011@gmail.com>
2025-05-30 20:49:18 +0100

    Implemented blog cards pagination, tag filter, and search funtionality (#17)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,16 @@ export default defineConfig({
     icon({
       include: { mdi: ["*"] },
     }),
+    pagefind(), // This should always be placed last so that all integrations run before it does
   ],
+  markdown: {
+    shikiConfig: {
+      // theme: "catppuccin-latte",
+      // theme: "night-owl",
+      themes: {
+        light: "catppuccin-latte",
+        dark: "night-owl",
+      },
+    },
+  },
 });
