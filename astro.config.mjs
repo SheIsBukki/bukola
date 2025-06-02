5020e002ff5897b7f6ece76caf27d6e98837ce0f
Bukola Ogunleye <bukolaogunleye2011@gmail.com>
2025-06-02 04:18:59 +0100

    Implemented reading time, auto-generated table of contents, and dynamic OG image——fingers crossed (#19)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import pagefind from "astro-pagefind";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,13 +19,18 @@ export default defineConfig({
     pagefind(), // This should always be placed last so that all integrations run before it does
   ],
   markdown: {
+    remarkPlugins: [
+      remarkReadingTime,
+      [remarkToc, { heading: "toc", maxDepth: 3 }],
+      // remarkPlugins: [ [remarkToc, { heading: "contents"} ] ],
+    ],
     shikiConfig: {
       // theme: "catppuccin-latte",
-      // theme: "night-owl",
-      themes: {
-        light: "catppuccin-latte",
-        dark: "night-owl",
-      },
+      theme: "night-owl",
+      // themes: {
+      //   light: "catppuccin-latte",
+      //   dark: "night-owl",
+      // },
     },
   },
 });
